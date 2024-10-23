516993653f83e2d11ab3777d13d61e6ffbb6f9fc
Ben Bruyns <43544274+BenStorm2514@users.noreply.github.com>
2024-10-23 09:41:07 -0500

    Update AllEventlyDB.sql
    Add authentication procedure

--- Documents/Database/AllEventlyDB.sql
+++ Documents/Database/AllEventlyDB.sql
@@ -3,69 +3,69 @@ CREATE DATABASE AllEventlyDB;
 
 -------------CREATE TABLES----------------
 --TABLE TO STORE USERS
-CREATE TABLE People {
-    email STRING,
-    first_name STRING NOT NULL,
-    last_name STRING NOT NULL,
+CREATE TABLE People (
+    email nvarchar(30),
+    first_name nvarchar(30) NOT NULL,
+    last_name nvarchar(30) NOT NULL,
     PRIMARY KEY(email)
-};
+);
 
 --TABLE TO STORE USER ACCOUNTS
-CREATE TABLE Accounts {
-    account_email STRING,
-    account_password STRING NOT NULL,
+CREATE TABLE Accounts (
+    account_email nvarchar(30),
+    account_password nvarchar(30) NOT NULL,
     PRIMARY KEY (email),
     FOREIGN KEY (account_email) REFERENCES People (email)
-};
+);
 
 --TABLE TO STORE ACCOUNT SESSIONS
-CREATE TABLE Sessions {
-    session_id STRING,
-    session_email STRING NOT NULL UNIQUE,
+CREATE TABLE Sessions (
+    session_id nvarchar(30),
+    session_email nvarchar(30) NOT NULL UNIQUE,
     PRIMARY KEY(session_id),
     FOREIGN KEY (session_email) REFERENCES Accounts (email)
-};
+);
 
 --TABLE TO STORE RESET PASSWORD CREDENTIALS
-CREATE TABLE Reset_Credentials {
-    account_email STRING,
-    temp_password STRING NOT NULL,
+CREATE TABLE Reset_Credentials (
+    account_email nvarchar(30),
+    temp_password nvarchar(30) NOT NULL,
     PRIMARY KEY (email),
     FOREIGN KEY (email) REFERENCES Accounts (email)
-};
+);
 
 --TABLE TO STORE IMAGE PATHS
-CREATE TABLE Image {
+CREATE TABLE Image (
     image_id INT,
-    image_path STRING NOT NULL,
+    image_path nvarchar(30) NOT NULL,
     is_template BOOLEAN DEFAULT FALSE,
     PRIMARY KEY (image_id)
-};
+);
 
 --TABLE TO STORE WHICH IMAGES BELONG TO WHICH USER
-CREATE TABLE User_Images {
+CREATE TABLE User_Images (
     image_id INT,
     owner_email INT NOT NULL,
     PRIMARY KEY (image_id),
     FOREIGN KEY (image_id) REFERENCES Images (image_id),
     FOREIGN KEY (owner_email) REFERENCES People (email)
-};
+);
 
 --TABLE TO STORE EVENTS
-CREATE TABLE Events {
+CREATE TABLE Events (
     event_id INT,
-    event_host STRING NOT NULL,
-    host_name STRING NOT NULL,
-    event_name STRING DEFAULT "My Event",
-    event_location: STRING NOT NULL,
+    event_host nvarchar(30) NOT NULL,
+    host_name nvarchar(30) NOT NULL,
+    event_name nvarchar(30) DEFAULT "My Event",
+    event_location: nvarchar(30) NOT NULL,
     event_start_date DATE DEFAULT GETDATE()+1,
     event_end_date DATE,
-    event_time_zone STRING DEFAULT "UTC",
-    invitation_layout STRING DEFAULT "Center",
+    event_time_zone nvarchar(30) DEFAULT "UTC",
+    invitation_layout nvarchar(30) DEFAULT "Center",
     background_image INT DEFAULT 0,
-    font_background_color STRING DEFAULT "#000000",
-    font_color STRING DEFAULT "#FFFFFF",
-    font STRING DEFAULT "italic bold 20px arial,serif",
+    font_background_color nvarchar(30) DEFAULT "#000000",
+    font_color nvarchar(30) DEFAULT "#FFFFFF",
+    font nvarchar(30) DEFAULT "italic bold 20px arial,serif",
     is_public BOOLEAN DEFAULT TRUE,
     reoccurs BOOLEAN DEFAULT FALSE,
     reoccur_freq INT,
@@ -77,24 +77,24 @@ CREATE TABLE Events {
     PRIMARY KEY (event_id),
     FOREIGN KEY (event_host) REFERENCES Accounts (email),
     FOREIGN KEY (background_image) REFERENCES Images (image_id)
-};
+);
 
 --TABLE TO STORE CHAT MESSAGES
-CREATE TABLE Chat_Messages {
+CREATE TABLE Chat_Messages (
     message_id INT,
     event_id INT NOT NULL,
-    sender_email STRING NOT NULL,
-    content STRING NOT NULL,
+    sender_email nvarchar(30) NOT NULL,
+    content nvarchar(30) NOT NULL,
     sent_date DATE DEFAULT GETDATE(),
     accepted BOOLEAN,
     PRIMARY KEY (message_id),
     FOREIGN KEY (event_id) REFERENCES EVENTS (event_id),
     FOREIGN KEY (sender_email) REFERENCES People (email)
-};
+);
 
 --TABLE TO STORE GUESTS OF EVENTS
-CREATE TABLE Guests {
-    guest_email STRING,
+CREATE TABLE Guests (
+    guest_email nvarchar(30),
     event_id INT,
     responded BOOLEAN DEFAULT FALSE,
     invite_sent BOOLEAN DEFAULT FALSE,
@@ -104,4 +104,16 @@ CREATE TABLE Guests {
     PRIMARY KEY (guest_email, event_id),
     FOREIGN KEY (guest_email) REFERENCES People(email),
     FOREIGN KEY (event_id) REFERENCES Events(event_id)
-};
+);
+
+-----------Create Procedures-------------
+CREATE PROCEDURE AuthenticateUser @Address nvarchar(30), @Pass nvarchar(30)
+AS
+    DECLARE @Accounts INT = SELECT COUNT(*) FROM Accounts
+    WHERE email = @Address and account_password = @Pass
+
+    IF @Accounts = 1 {
+        INSERT INTO Sessions ()
+    }
+GO;
+-----------Create Triggers---------------
