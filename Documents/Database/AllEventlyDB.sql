506ab090c2e6ad37728ba933f7becf35774283ba
Ben Bruyns <43544274+BenStorm2514@users.noreply.github.com>
2024-10-28 10:28:25 -0500

    Create Procedures
    Authentication & password resets

--- Documents/Database/AllEventlyDB.sql
+++ Documents/Database/AllEventlyDB.sql
@@ -107,13 +107,129 @@ CREATE TABLE Guests (
 );
 
 -----------Create Procedures-------------
-CREATE PROCEDURE AuthenticateUser @Address nvarchar(30), @Pass nvarchar(30)
+--Authenticate Users
+CREATE PROCEDURE Authenticate_User
+@Email_Address nvarchar(30), @Pass nvarchar(30)
 AS
-    DECLARE @Accounts INT = SELECT COUNT(*) FROM Accounts
-    WHERE email = @Address and account_password = @Pass
+    BEGIN
+        --Get count of accounts with given email and password
+        DECLARE @Count_A INT = SELECT COUNT(*) FROM Accounts
+        HAVING email = @Email_Address and account_password = @Pass;
 
-    IF @Accounts = 1 {
-        INSERT INTO Sessions ()
-    }
+        --Check if there is only one account
+        IF @Count_A = 1 {
+            --Create Session
+            INSERT INTO Sessions (session_email)
+            VALUES (@Email_Address)
+
+            --Get Session ID
+            SELECT session_id FROM Sessions
+            WHERE session_email = @Email_Address;
+        }
+    END
 GO;
+
+--Add Account
+CREATE PROCEDURE Create_Account
+@Email_Address nvarchar(30), @Pass nvarchar(30), @FName nvarchar(30), @LName nvarchar(30)
+AS
+    BEGIN
+        --Get count of people with given address
+        DECLARE @Count_P INT = SELECT COUNT(*) FROM People
+        HAVING email = @Email_Address
+
+        --Get count of accounts with given address
+        DECLARE @Count_A INT = SELECT COUNT(*) FROM Accounts
+        HAVING email = @Email_Address
+
+        --Check if no account exists
+        IF @Count_A = 0 {
+            IF @Count_P = 0 {
+                --If no person exists, make one
+                INSERT INTO People (email, first_name, last_name)
+                VALUES (@Email_Address, @FName, @LName)
+            } ELSE IF Count_P = 1 {
+                --If a person exists, update them
+                UPDATE People
+                SET first_name = @FName, last_name = @LName
+                WHERE email = @Email_Address
+            }
+
+            --Create Account
+            INSERT INTO Accounts (account_email, account_password)
+            VALUES (@Email_Address, @Pass)
+
+            --Create Session
+            INSERT INTO Sessions (session_email)
+            VALUES (@Email_Address)
+
+            --Get Session ID
+            SELECT session_id FROM Sessions
+            WHERE session_email = @Email_Address;
+        }
+    END
+GO;
+
+--Create reset credentials
+CREATE PROCEDURE Create_Reset_Credentials
+@Email_Address nvarchar(30)
+AS
+    BEGIN
+
+        --Get count of accounts with given address in Accounts
+        DECLARE @Count_A INT = SELECT COUNT(*) FROM Accounts
+        HAVING account_email = @Email_Address
+
+        --Get count of accounts with given address in Reset_Credentials
+        DECLARE @Count_RC INT = SELECT COUNT(*) FROM Reset_Credentials
+        HAVING account_email = @Email_Address
+
+        IF (@Count_A = 1 AND @Count_RC < 1) {
+
+            DECLARE @rand_num INT = SELECT FLOOR(RAND()*9999999999)
+
+            DECLARE @temp nvarchar(30) = CAST(@rand_num AS nvarchar(30))
+
+            WHILE LEN(@temp) < 8 {
+                DECLARE @diff INT = 8 - LEN(@temp)
+
+                @rand_num = SELECT FLOOR(RAND()*(POWER(10, @diff)-1))
+
+                @temp = @temp + CAST(@rand_num AS nvarchar(30))
+            }
+
+            INSERT INTO Reset_Credentials (account_email, temp_password)
+            VALUES (@Email_Address, @temp)
+
+            SELECT account_email, temp_password FROM Reset_Credentials
+            where account_email = @Email_Address
+        }
+    END
+GO;
+
+--Reset Password
+CREATE PROCEDURE Update_Password
+@Email_Address nvarchar(30), @temp nvarchar(30), @new_pass nvarchar(30)
+AS
+    BEGIN
+        --Get count of accounts with given address in Reset_Credentials
+        DECLARE @Count_RC INT = SELECT COUNT(*) FROM Reset_Credentials
+        HAVING account_email = @Email_Address AND temp_password = @temp
+
+        IF(@Count_RC = 1) {
+
+            DELETE FROM Reset_Credentials
+            WHERE account_email = @Email_Address
+
+            UPDATE Accounts
+            SET account_password = @new_pass
+            WHERE account_email = @Email_Address
+
+        }
+    END
+GO;
+
+--Get User Events
+
+
 -----------Create Triggers---------------
