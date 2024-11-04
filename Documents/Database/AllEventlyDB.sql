756ff297a0171f1a1652ab4e05ca67d3ebfa6ec7
Ben Bruyns <43544274+BenStorm2514@users.noreply.github.com>
2024-11-04 10:57:32 -0600

    Postgresize the functions

--- Documents/Database/AllEventlyDB.sql
+++ Documents/Database/AllEventlyDB.sql
@@ -1,6 +1,3 @@
---CREATE DATABASE
-CREATE DATABASE AllEventlyDB;
-
 -------------CREATE TABLES----------------
 --TABLE TO STORE USERS
 CREATE TABLE People (
@@ -106,152 +103,178 @@ CREATE TABLE Guests (
     FOREIGN KEY (event_id) REFERENCES Events(event_id)
 );
 
------------Create Procedures-------------
+-----------Create Functions-------------
 --Authenticate Users
-CREATE FUNCTION Authenticate_User
-(Email_Address varchar(30), Pass varchar(30)) RETURNS INT
-LANGUAGE SQL
+CREATE OR REPLACE FUNCTION Authenticate_User
+(Email_Address varchar(30), Pass varchar(30)) RETURNS INT AS $$
+DECLARE
+	Count_A INT;
 BEGIN
-    --Get count of accounts with given email and password
-    Count_A := (SELECT COUNT(*) FROM Accounts
-    HAVING email = Email_Address and account_password = Pass)
+
+	Count_A := (SELECT COUNT(*) FROM Accounts
+    HAVING email = Email_Address and account_password = Pass);
 
     --Check if there is only one account
-    IF Count_A = 1
+    IF Count_A = 1 THEN
         --Create Session
         INSERT INTO Sessions (session_email)
-        VALUES (Email_Address)
+        VALUES (Email_Address);
 
         --Get Session ID
-        SELECT session_id FROM Sessions
-        WHERE session_email = Email_Address;
-END;
+        RETURN (SELECT session_id FROM Sessions
+        WHERE session_email = Email_Address);
+	END IF;
+
+	RETURN NULL;
+	
+END $$ LANGUAGE plpgsql;
 
 --Add Account
-CREATE PROCEDURE Create_Account
-(Email_Address varchar(30), Pass varchar(30), FName varchar(30), LName varchar(30))
-AS
-    BEGIN
-        --Get count of people with given address
-        DECLARE Count_P INT = (SELECT COUNT(*) FROM People
-        HAVING email = Email_Address)
+CREATE OR REPLACE FUNCTION Create_Account
+(Email_Address varchar(30), Pass varchar(30), FName varchar(30), LName varchar(30)) RETURNS INT AS $$
+DECLARE
+	Count_P INT;
+	Count_A INT;
+BEGIN
+    --Get count of people with given address
+	Count_P := (SELECT COUNT(*) FROM People
+    HAVING email = Email_Address);
 
-        --Get count of accounts with given address
-        DECLARE Count_A INT = (SELECT COUNT(*) FROM Accounts
-        HAVING email = Email_Address)
+	--Get count of accounts with given address
+    Count_A := (SELECT COUNT(*) FROM Accounts
+	HAVING email = Email_Address);
 
-        --Check if no account exists
-        IF Count_A = 0
-            IF Count_P = 0
-                --If no person exists, make one
-                INSERT INTO People (email, first_name, last_name)
-                VALUES (Email_Address, FName, LName)
-            ELSE IF Count_P = 1
-                --If a person exists, update them
-                UPDATE People
-                SET first_name = FName, last_name = LName
-                WHERE email = Email_Address
+    --Check if no account exists
+    IF Count_A = 0 THEN
+        IF Count_P = 0 THEN
+        	--If no person exists, make one
+            INSERT INTO People (email, first_name, last_name)
+            VALUES (Email_Address, FName, LName);
+        ELSIF Count_P = 1 THEN
+            --If a person exists, update them
+            UPDATE People
+            SET first_name = FName, last_name = LName
+            WHERE email = Email_Address;
+		END IF;
 
-            --Create Account
-            INSERT INTO Accounts (account_email, account_password)
-            VALUES (Email_Address, Pass)
+        --Create Account
+        INSERT INTO Accounts (account_email, account_password)
+        VALUES (Email_Address, Pass);
 
-            --Create Session
-            INSERT INTO Sessions (session_email)
-            VALUES (Email_Address)
+        --Create Session
+        INSERT INTO Sessions (session_email)
+        VALUES (Email_Address);
 
-            --Get Session ID
-            SELECT session_id FROM Sessions
-            WHERE session_email = Email_Address;
+        --Get Session ID
+        RETURN (SELECT session_id FROM Sessions
+        WHERE session_email = Email_Address);
+	END IF;
 
-    END
-GO;
+	RETURN NULL;
+	
+END $$ LANGUAGE plpgsql;
+
 
 --Create reset credentials
-CREATE PROCEDURE Create_Reset_Credentials
-(Email_Address varchar(30))
-AS
-    BEGIN
+CREATE OR REPLACE FUNCTION Create_Reset_Credentials
+(Email_Address varchar(30)) RETURNS varchar(30) AS $$
+DECLARE
+	Count_A INT;
+	Count_RC INT;
+	rand_num INT;
+	temp_pass varchar(30);
+	diff INT;
+BEGIN
 
-        --Get count of accounts with given address in Accounts
-        DECLARE Count_A INT = (SELECT COUNT(*) FROM Accounts
-        HAVING account_email = Email_Address)
+    --Get count of accounts with given address in Accounts
+    Count_A := (SELECT COUNT(*) FROM Accounts
+    HAVING account_email = Email_Address);
 
-        --Get count of accounts with given address in Reset_Credentials
-        DECLARE Count_RC INT = (SELECT COUNT(*) FROM Reset_Credentials
-        HAVING account_email = Email_Address)
+    --Get count of accounts with given address in Reset_Credentials
+    Count_RC := (SELECT COUNT(*) FROM Reset_Credentials
+    HAVING account_email = Email_Address);
 
-        IF (Count_A = 1 AND Count_RC < 1)
+    IF (Count_A = 1 AND Count_RC < 1) THEN
 
-            DECLARE rand_num INT = (SELECT FLOOR(RAND()*9999999999))
+        rand_num := (SELECT FLOOR(RAND()*9999999999));
 
-            DECLARE temp varchar(30) = CAST(rand_num AS varchar(30))
+        temp_pass := CAST(rand_num AS varchar(30));
 
-            WHILE LEN(temp) < 8
-                DECLARE diff INT = 8 - LEN(temp)
+        WHILE (LEN(temp_pass) < 8) LOOP
+            diff := 8 - LEN(temp_pass);
 
-                SET rand_num = (SELECT FLOOR(RAND()*(POWER(10, diff)-1)))
+            rand_num := (SELECT FLOOR(RAND()*(POWER(10, diff)-1)));
 
-                SET temp = temp + CAST(rand_num AS varchar(30))
+            temp_pass := temp_pass + CAST(rand_num AS varchar(30));
+		END LOOP;
 
-            INSERT INTO Reset_Credentials (account_email, temp_password)
-            VALUES (Email_Address, temp)
+        INSERT INTO Reset_Credentials (account_email, temp_password)
+        VALUES (Email_Address, temp);
 
-            SELECT account_email, temp_password FROM Reset_Credentials
-            where account_email = Email_Address
+        RETURN (SELECT temp_password FROM Reset_Credentials
+        where account_email = Email_Address);
+	END IF;
 
-    END
-GO;
+	RETURN NULL;
+
+END $$ LANGUAGE plpgsql;
+
 
 --Reset Password
-CREATE PROCEDURE Update_Password
+CREATE OR REPLACE FUNCTION Update_Password
 (Email_Address varchar(30), temp varchar(30), new_pass varchar(30))
-AS
-    BEGIN
-        --Get count of accounts with given address in Reset_Credentials
-        DECLARE Count_RC INT = (SELECT COUNT(*) FROM Reset_Credentials
-        HAVING account_email = Email_Address AND temp_password = temp)
+RETURNS INT AS $$
+DECLARE
+	Count_RC INT;
+BEGIN
+    --Get count of accounts with given address in Reset_Credentials
+    Count_RC := (SELECT COUNT(*) FROM Reset_Credentials
+    HAVING account_email = Email_Address AND temp_password = temp);
 
-        IF (Count_RC = 1)
-            DELETE FROM Reset_Credentials
-            WHERE account_email = Email_Address
+    IF (Count_RC = 1) THEN
+        DELETE FROM Reset_Credentials
+        WHERE (account_email = Email_Address);
 
-            UPDATE Accounts
-            SET account_password = new_pass
-            WHERE account_email = Email_Address
-    END
-GO;
+        UPDATE Accounts
+        SET account_password = new_pass
+        WHERE (account_email = Email_Address);
+    END IF;
+	
+END $$ LANGUAGE plpgsql;
+
 
 --Get Invited Events
-CREATE PROCEDURE Get_Attending_Events
+CREATE OR REPLACE FUNCTION Get_Attending_Events
 (Email_Address varchar(30))
-AS
-    SELECT event_id, event_host, host_name, event_name, event_location, event_start_date, event_end_date, is_public,
-    reoccurs, reoccur_freq, end_reocurrs, request_child_num, notify_host, max_additional_guests, limit_additional_guests,
-    font, font_color, font_background_color, image_path
-    FROM Events AS E JOIN Images AS I ON E.image_id = I.image_id
-    WHERE event_id IN (SELECT ARRAY_AGG(event_id) FROM Guests
-        WHERE guest_email = Email_Address)
-GO;
+RETURNS SETOF Events AS $BODY$
+BEGIN
+
+    RETURN NEXT (SELECT * FROM Events
+		WHERE event_id IN (SELECT ARRAY_AGG(event_id) FROM Guests
+        	WHERE guest_email = Email_Address));
+		
+END $BODY$ LANGUAGE plpgsql;
 
 --Get Hosted Events
-CREATE PROCEDURE Get_Hosted_Events
+CREATE OR REPLACE FUNCTION Get_Hosted_Events
 (Email_Address varchar(30))
-AS
-    SELECT event_id, event_host, host_name, event_name, event_location, event_start_date, event_end_date, is_public,
-    reoccurs, reoccur_freq, end_reocurrs, request_child_num, notify_host, max_additional_guests, limit_additional_guests,
-    font, font_color, font_background_color, image_path FROM Events
-    WHERE event_host = Email_Address
-GO;
+RETURNS SETOF Events AS $BODY$
+BEGIN
+
+    RETURN NEXT (SELECT * FROM Events
+    WHERE event_host = Email_Address);
+	
+END $BODY$ LANGUAGE plpgsql;
 
 --Get Public Events
-CREATE PROCEDURE Get_Public_Events
-AS
-    SELECT event_id, event_host, host_name, event_name, event_location, event_start_date, event_end_date, is_public,
-    reoccurs, reoccur_freq, end_reocurrs, request_child_num, notify_host, max_additional_guests, limit_additional_guests,
-    font, font_color, font_background_color, image_path FROM Events
-    WHERE is_public = TRUE
-GO;
+CREATE OR REPLACE FUNCTION Get_Public_Events ()
+RETURNS SETOF Events AS $BODY$
+BEGIN
+
+    SELECT * FROM Events
+    WHERE is_public = TRUE;
+	
+END $BODY$ LANGUAGE plpgsql;
 
 
 -----------Create Triggers---------------
