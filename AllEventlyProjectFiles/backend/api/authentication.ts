4a1d726620b85a5cf5117da59c87184ee9e1c78f
Spenser Morey <spensermorey@Spensers-MacBook-Pro.local>
2024-12-02 10:30:46 -0600

    Commented out fetchUser

--- AllEventlyProjectFiles/backend/api/authentication.ts
+++ AllEventlyProjectFiles/backend/api/authentication.ts
@@ -54,14 +54,10 @@ const handler = async (req: IncomingMessage, res: ServerResponse): Promise<void>
                 return;
             }
             const result = await pool.query('SELECT AUTHENTICATE_USER($1, $2);', [body.email, body.password]);
-            if (result.rows.length === 0 || result.rows[0].authenticate_user === null) {
+            if (result.rows.length === 0 || result.rows[0].authenticate_user === null || result.rows[0] === null) {
                 res.statusCode = 401;
                 res.end(JSON.stringify({message : 'Invalid Credentials'}));
                 return;
-            }
-            if (result === null || result === undefined){
-                res.statusCode = 401;
-                res.end(JSON.stringify({message : 'Invalid Credentials'}));
             }
             const { authenticate_user: sessionId } = result.rows[0];
             if (sessionId){
